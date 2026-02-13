2b7a0facbddd69abfeab58b73ef6763fb8c7664d
Md Badiuzzaman Pranto <prantoamt@gmail.com>
2026-02-13 22:21:57 +0100

    chore: tests removed from release pipeline

--- src/djsuite/templates/common/tests/test_health.py
+++ src/djsuite/templates/common/tests/test_health.py
@@ -6,6 +6,7 @@ from rest_framework.test import APIClient
 
 @pytest.mark.django_db
 def test_health_check_returns_200(api_client: APIClient) -> None:
+    """Verify the health endpoint returns 200 with status 'healthy' when the database is reachable."""
     url = reverse("health-check")
     response = api_client.get(url)
     assert response.status_code == HTTP_200_OK
